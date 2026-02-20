86d03a02ae157d47e4abbbbce2dfae331d70c343
Duyen Le <kieuduyenle1810@gmail.com>
2026-02-20 22:06:11 +0700

    Them read - hien thi sach

--- src/main/java/com/mycompany/projectjava2/controller/ViewController.java
+++ src/main/java/com/mycompany/projectjava2/controller/ViewController.java
@@ -3,7 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
 package com.mycompany.projectjava2.controller;
-
+import com.mycompany.projectjava2.database.BookDAO;
+import com.mycompany.projectjava2.model.Book;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -11,8 +12,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 /**
  * FXML Controller class
@@ -20,6 +21,7 @@ import javafx.scene.control.TextField;
  * @author WINDOWS
  */
 public class ViewController implements Initializable {
+    BookDAO book = new BookDAO();
 
     @FXML
     private TextField titleField;
@@ -30,28 +32,27 @@ public class ViewController implements Initializable {
     @FXML
     private TextField searchField;
     @FXML
-    private TableView<?> bookTable;
-    @FXML
-    private TableColumn<?, ?> colId;
-    @FXML
-    private TableColumn<?, ?> colTitle;
-    @FXML
-    private TableColumn<?, ?> colAuthor;
-    @FXML
-    private TableColumn<?, ?> colPrice;
-
-    @FXML
     private TextField catField;
     @FXML
     private TextField qtyField;
     @FXML
     private TextField pbField;
     @FXML
-    private TableColumn<?, ?> colcat;
+    private TableView<Book> bookTable;
     @FXML
-    private TableColumn<?, ?> colQty;
+    private TableColumn<Book, Integer> colId;
     @FXML
-    private TableColumn<?, ?> colPub;
+    private TableColumn<Book, String> colTitle;
+    @FXML
+    private TableColumn<Book, String> colAuthor;
+    @FXML
+    private TableColumn<Book, Double> colPrice;
+    @FXML
+    private TableColumn<Book, String> colcat;
+    @FXML
+    private TableColumn<Book, Integer> colQty;
+    @FXML
+    private TableColumn<Book, Integer> colPub;
 
     /**
      * Initializes the controller class.
@@ -61,14 +62,31 @@ public class ViewController implements Initializable {
 
         // TODO
         bookTable.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
-
         colId.prefWidthProperty().bind(bookTable.widthProperty().multiply(0.08));
-        colTitle.prefWidthProperty().bind(bookTable.widthProperty().multiply(0.32));
+        colTitle.prefWidthProperty().bind(bookTable.widthProperty().multiply(0.37));
         colcat.prefWidthProperty().bind(bookTable.widthProperty().multiply(0.15));
         colAuthor.prefWidthProperty().bind(bookTable.widthProperty().multiply(0.17));
         colPrice.prefWidthProperty().bind(bookTable.widthProperty().multiply(0.10));
         colQty.prefWidthProperty().bind(bookTable.widthProperty().multiply(0.08));
-        colPub.prefWidthProperty().bind(bookTable.widthProperty().multiply(0.10));
+        colPub.prefWidthProperty().bind(bookTable.widthProperty().multiply(0.05));
+        
+        colId.setCellValueFactory(new PropertyValueFactory<>("bookId"));
+        colId.setStyle("-fx-alignment: CENTER;");
+        colTitle.setCellValueFactory(new PropertyValueFactory<>("title"));
+        colcat.setCellValueFactory(new PropertyValueFactory<>("category"));
+        colAuthor.setCellValueFactory(new PropertyValueFactory<>("author"));
+        colPrice.setCellValueFactory(new PropertyValueFactory<>("price"));
+        colPrice.setStyle("-fx-alignment: CENTER-RIGHT;");
+        colQty.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+        colQty.setStyle("-fx-alignment: CENTER;");
+        colPub.setCellValueFactory(new PropertyValueFactory<>("publishYear"));
+        colPub.setStyle("-fx-alignment: CENTER;");
+        handleDisplay();
+    }
+    
+    @FXML
+    private void handleDisplay() {      
+        bookTable.setItems(book.getAllBooks());
     }
 
     @FXML
@@ -94,5 +112,7 @@ public class ViewController implements Initializable {
     @FXML
     private void handleDelete(ActionEvent event) {
     }
+    
+    
 
 }
--- src/main/java/com/mycompany/projectjava2/database/BookDAO.java
+++ src/main/java/com/mycompany/projectjava2/database/BookDAO.java
@@ -4,10 +4,51 @@
  */
 package com.mycompany.projectjava2.database;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import com.mycompany.projectjava2.model.Book;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author WINDOWS
  */
 public class BookDAO {
     //SQL + mapping
+    Connection cn = DBConnection.getConnect();
+    Statement st;
+    ResultSet rs;
+    
+    public ObservableList<Book> getAllBooks() {
+        
+        ObservableList<Book> list = FXCollections.observableArrayList();
+
+        //viet cau lenh truy van
+        String query = "SELECT * FROM books";
+        try {
+            //tao statement
+            st = cn.createStatement();
+            //thuc thi - execute
+            rs = st.executeQuery(query);
+            //doc du lieu
+            while(rs.next())
+            {
+                list.add(new Book(
+                    rs.getInt("book_id"),
+                    rs.getString("title"),
+                    rs.getString("author"),
+                    rs.getString("category"),
+                    rs.getDouble("price"),
+                    rs.getInt("quantity"),
+                    rs.getInt("publish_year")
+                ));
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(BookDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list;
+    }
 }
